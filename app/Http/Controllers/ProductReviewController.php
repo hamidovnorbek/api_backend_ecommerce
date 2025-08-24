beca37fe6a2be8aa6a7f0e6bf35332f1994e576e
hamidovnorbek <norbekha@gmail.com>
2025-08-24 20:17:26 +0500

    giving reviews & rating for products

--- app/Http/Controllers/ProductReviewController.php
+++ app/Http/Controllers/ProductReviewController.php
@@ -4,13 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreReviewRequest;
 use App\Http\Requests\StoreStatusRequest;
+use App\Http\Resources\ReviewResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductReviewController extends Controller
 {
+    public function index(Product $product)
+    {
+        return response([
+           'overall_rating' => $product->reviews()->avg('rating'),
+           'reviews_count' => $product->reviews()->count(),
+           'reviews' => ReviewResource::collection($product->reviews()->latest()->paginate(10)),
+        ]);
+    }
     public function store(Product $product, StoreReviewRequest $request)
     {
+        $product->reviews()->create([
+            'user_id' => auth()->user()->id,
+            'rating' => $request->rating,
+            'body' => $request->body
+        ]);
 
+        return response([
+            'success' => true,
+            'message' => 'Your review has been created'
+        ]);
     }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -18,4 +18,9 @@ class Review extends Model
         return $this->belongsTo(Product::class);
     }
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
